61b1b015d63474d3a8db377db20797a124e8e921
pp-ps <pietro.stroia@pagopa.it>
2023-06-19 10:03:37 +0200

    pagination stub

--- explore.py
+++ explore.py
@@ -3,12 +3,15 @@
 # Usage: set SLACK_API_TOKEN env variable and run `python3 explore.py`
 # Slack OAuth Scopes needed: channels:read, users:read, channels:history, groups:read
 import os
+import time
+
 from slack_sdk import WebClient
 
 CHANNEL_TYPES = ["public_channel"]
 
 
 def get_channel_creator(channel_id, web_client):
+    time.sleep(2)
     channel_info = web_client.conversations_info(channel=channel_id)
     if not channel_info["ok"]:
         print(f"Failed to retrieve channel info for channel: {channel_id}")
@@ -22,7 +25,9 @@ def get_channel_creator(channel_id, web_client):
         print(f"Failed to retrieve creator info for channel: {channel_id}")
         return None
 
-    return creator_info["user"]["real_name"], creator_info["user"]["name"]
+    real_name = creator_info.get("user").get("real_name")
+    name = creator_info.get("user").get("name")
+    return real_name, name
 
 
 def main():
@@ -32,16 +37,27 @@ def main():
     ), "SLACK_API_TOKEN is not set. Check your SLACK_API_TOKEN env variable."
 
     web_client = WebClient(token=access_token)
-    response = web_client.conversations_list(types=CHANNEL_TYPES)
-    if not response.get("ok"):
-        return response.get("error")
+    channels = []
+    continue_paging = True
+    cursor = None
+    while(continue_paging):
+        print(f"[DEBUG] paging cursor: {cursor}")
+        time.sleep(2)
+        response = web_client.conversations_list(types=CHANNEL_TYPES, cursor=cursor)
+        if not response.get("ok"):
+            return response.get("error")
+        cursor = response.get("response_metadata").get("next_cursor")
+        if not cursor or cursor == "":
+            continue_paging = False
+        channels_page = response.get("channels")
+        if channels_page:
+            channels.extend(channels_page)
 
-    channels = response.get("channels")
     for channel in channels:
+        print(f"[DEBUG] Querying {channel['name']}")
         creator_name, creator_real_name = get_channel_creator(channel["id"], web_client)
         if creator_name:
             print(f"{channel['name']}, {creator_name}, {creator_real_name}")
-
 
 if __name__ == "__main__":
     main()
