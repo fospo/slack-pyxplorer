380639214db2d9ee2717f64272c41e319b93d06c
libremente <surf@libremente.eu>
2023-06-29 11:22:13 +0200

    chore: added sleep and pagination to avoid rate limiting

--- explore.py
+++ explore.py
@@ -2,10 +2,14 @@
 # Pre-requisites: Python 3.6+, slack_sdk -> pip3 install slack_sdk
 # Usage: set SLACK_API_TOKEN env variable and run `python3 explore.py`
 # Slack OAuth Scopes needed: channels:read, users:read, channels:history, groups:read
+# Using sleep and pagination to avoid rate limiting, see https://api.slack.com/docs/rate-limits
 import os
+import time
 from slack_sdk import WebClient
 
 CHANNEL_TYPES = ["public_channel"]
+CHANNELS_PER_PAGE = 50
+SLEEP_TIME = 2
 
 
 def get_channel_creator(channel_id, web_client):
@@ -14,9 +18,6 @@ def get_channel_creator(channel_id, web_client):
         print(f"Failed to retrieve channel info for channel: {channel_id}")
         return None
 
-    # There are several ways to get the 'name' of the creator
-    # 'real_name' should be the one to tag for a message, let's see
-    # See https://api.slack.com/methods/users.info for more info
     creator_info = web_client.users_info(user=channel_info["channel"]["creator"])
     if not creator_info["ok"]:
         print(f"Failed to retrieve creator info for channel: {channel_id}")
@@ -25,22 +26,33 @@ def get_channel_creator(channel_id, web_client):
     return creator_info["user"]["name"], creator_info["user"]["real_name"]
 
 
+def process_channel(channel, web_client):
+    creator_name, creator_real_name = get_channel_creator(channel["id"], web_client)
+    if creator_name:
+        print(f"{channel['name']}, {creator_name}, {creator_real_name}")
+
+
 def main():
     access_token = os.getenv("SLACK_API_TOKEN")
-    assert (
-        access_token
-    ), "SLACK_API_TOKEN is not set. Check your SLACK_API_TOKEN env variable."
+    assert access_token, "SLACK_API_TOKEN is not set. Check your SLACK_API_TOKEN env variable."
 
     web_client = WebClient(token=access_token)
-    response = web_client.conversations_list(types=CHANNEL_TYPES)
-    if not response.get("ok"):
-        return response.get("error")
+    cursor = None
 
-    channels = response.get("channels")
-    for channel in channels:
-        creator_name, creator_real_name = get_channel_creator(channel["id"], web_client)
-        if creator_name:
-            print(f"{channel['name']}, {creator_name}, {creator_real_name}")
+    while True:
+        time.sleep(SLEEP_TIME)
+        print(f"Processing cursor: {cursor}")
+        channel_list_response = web_client.conversations_list(types=CHANNEL_TYPES, limit=CHANNELS_PER_PAGE, cursor=cursor)
+        if not channel_list_response.get("ok"):
+            print(channel_list_response.get("error"))
+            break
+
+        channels = channel_list_response.get("channels")
+        [process_channel(channel, web_client) for channel in channels]
+
+        cursor = channel_list_response.get("response_metadata", {}).get("next_cursor")
+        if not cursor:
+            break
 
     exit(0)
 
